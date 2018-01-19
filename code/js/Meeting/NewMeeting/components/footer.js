964f990a7ba9076c93d6c03b43a6702b81c09c49
SkKostya <skripkin_kostyunya@mail.ru>
2018-01-19 20:01:48 +0600

    Fixed bugs

--- code/js/Meeting/NewMeeting/components/footer.js
+++ code/js/Meeting/NewMeeting/components/footer.js
@@ -2,6 +2,9 @@ import React from "react";
 
 export function Footer () {
 	return (
-		
+		<div className="footer1">
+			<button className="footer1__cancle">Отмена</button>
+			<button className="footer1__creature">Создать встречу</button>
+		</div>
 	);
 };
--- code/js/Wrapper/wrapper.js
+++ code/js/Wrapper/wrapper.js
@@ -3,6 +3,8 @@ import React, { Component } from "react";
 import Header from "../Header/index";
 import Main from "../Main/index";
 
+import Meeting from "../Meeting/index";
+
 
 class Wrapper extends Component {
 	render () {
